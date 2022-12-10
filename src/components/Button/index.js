28b0c767cfbec040442963f062768af4d46f3bcf
adriano.dev <adriano.coelho@dataside.com.br>
2022-12-09 21:12:01 -0300

    desafio-github

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { ButtonContainer } from "./styles";
+
+function Button({ onClick }) {
+  return <ButtonContainer onClick={onClick}>Buscar</ButtonContainer>;
+}
+
+export default Button;
